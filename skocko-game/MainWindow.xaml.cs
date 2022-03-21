8104c391a0ed0484d2fd96f49542ae311336353b
Ensar <ensarhamzic01@gmail.com>
2022-03-21 14:58:03 +0100

    Add project files.

---
+++ skocko-game/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace skocko_game
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //foreach (object child in r0.Children)
+            //{
+            //    if (child is Grid)
+            //    {
+            //        Grid ch = child as Grid;
+            //        foreach(object rect in ch.Children)
+            //        {
+            //            if(rect is Rectangle)
+            //            {
+            //                Rectangle r = (Rectangle)rect;
+            //                MessageBox.Show(r.Height.ToString());
+            //            }
+            //        }
+            //    }
+            //}
+        }
+    }
+}
